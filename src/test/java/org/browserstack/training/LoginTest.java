19c292c080daa6cdee595e3fe0cbfd37740737a3
Gary Behan <gary.b@browserstack.com>
2025-08-25 16:38:59 +0100

    Initial Commit

--- src/test/java/org/browserstack/training/LoginTest.java
+++ src/test/java/org/browserstack/training/LoginTest.java
@@ -1,7 +1,6 @@
 package org.browserstack.training;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,7 +9,7 @@ import org.testng.annotations.Test;
 public class LoginTest {
 
     @Test
-    public void loginTest() {
+    public void successfulLoginTest() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
@@ -30,8 +29,55 @@ public class LoginTest {
             driver.quit();
             System.out.println("There was an error: " + e);
         }
-
-
     }
 
+    @Test
+    public void failedLoginTestUsername() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+            driver.manage().window().maximize();
+
+            driver.findElement(By.cssSelector("#username")).sendKeys("wrongStudent");
+            driver.findElement(By.cssSelector("#password")).sendKeys("Password123");
+            driver.findElement(By.cssSelector("#submit")).click();
+
+            Thread.sleep(3000);
+
+            assert driver.findElement(By.cssSelector("#error")).getText().equals("Your username is invalid!");
+
+            driver.quit();
+        } catch (Exception e) {
+            driver.quit();
+            System.out.println("There was an error: " + e);
+        }
+    }
+
+    @Test
+    public void failedLoginTestPassword() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+            driver.manage().window().maximize();
+
+            driver.findElement(By.cssSelector("#username")).sendKeys("student");
+            driver.findElement(By.cssSelector("#password")).sendKeys("Password456");
+            driver.findElement(By.cssSelector("#submit")).click();
+
+            Thread.sleep(3000);
+
+            assert driver.findElement(By.cssSelector("#error")).getText().equals("Your password is invalid!");
+
+            driver.quit();
+        } catch (Exception e) {
+            driver.quit();
+            System.out.println("There was an error: " + e);
+        }
+    }
 }
